63df8787fa10944687053cf7f8f21685a5d806ab
sldanischulz <dfaria@umich.edu>
2025-04-10 10:56:18 -0400

    Step by step breakdown

--- final_project.py
+++ final_project.py
@@ -6,3 +6,32 @@
 #   - Oleg Korobkov
 #   - Danielle Schulz (dfaria@umich.edu - UMID63218489)
 
+'''
+Abstract:
+API's:
+    - Truthbrush
+    - Market one
+    - Trendy word one
+
+Step by step: (this is for me to check myself and see if I understand what's up)
+  FIRST ANALYSIS: The correlation between Trump's posts and the Stock Market  
+    1. Pull info from truthbrush API
+    2. Understand output
+    3. Pull info from market one API
+    4. Limit output to 25, figure out how to pull from multiple different dates for both data bases
+    5. Determine timeframe for analysis
+    6. Join databaframes based on time 
+    7. Calculate correlation
+    8. Plot maps
+    
+  SECOND ANALYSIS  
+    1. Break posts data into a word count dictionary
+    2. Run top something words into Google Trends API
+    3. Identify the posts that contain the trendiest words
+    4. Compare those posts and stock numbers with the ones with least trendy words
+    5. Plot the results
+    6. Write up the analysis and conclusions
+    7. Boot
+    8. Rally
+
+'''
